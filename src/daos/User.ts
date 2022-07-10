89396c5a869dfe8f51bf67074ad4e285ef43dc5f
FranciscoMessina <francisco@messina.ws>
2022-07-10 12:46:03 -0300

    added a few files and folder to better organize in layers

--- src/daos/User.ts
+++ src/daos/User.ts
@@ -1,35 +1,33 @@
-import { UserModel as UserModel } from "../models";
-
+import { UserModel } from '../models';
 
 export interface IUser {
-   id?: string;
-   email: string;
-   password: string;
+  id?: string;
+  email: string;
+  password: string;
 }
 
 export class User {
-   static async create(data: IUser) {
-      return UserModel.create(data)
-   }
+  static async create(data: IUser) {
+    return UserModel.create(data);
+  }
 
+  static async getAll() {
+    return UserModel.find();
+  }
 
-   static async getAll() {
-      return UserModel.find()
-   }
+  static async getById(id: string) {
+    return UserModel.findById(id);
+  }
 
-   static async getById(id: string) {
-      return UserModel.findById(id)
-   }
+  static async getByEmail(email: string) {
+    return UserModel.findOne({ email });
+  }
 
-   static async getByEmail(email: string) {
-      return UserModel.findOne({ email})
-   }
+  static async update(id: string, data: IUser) {
+    return UserModel.findByIdAndUpdate(id, data, { new: true });
+  }
 
-   static async update(id: string, data: IUser) {
-      return UserModel.findByIdAndUpdate(id, data, { new: true })
-   }
-
-   static async delete(id: string) { 
-      return UserModel.findByIdAndDelete(id)
-   }
+  static async delete(id: string) {
+    return UserModel.findByIdAndDelete(id);
+  }
 }
